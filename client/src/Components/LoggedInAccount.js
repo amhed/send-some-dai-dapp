adbe572ed2c833d8fee356d9d53531c54bfdb793
Amhed <amhed@harbor.com>
2018-09-08 14:45:18 +0200

    Some styles

--- client/src/Components/LoggedInAccount.js
+++ client/src/Components/LoggedInAccount.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-class LoggedInAccount extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const Identicon = props => (
+  <img
+    style={{borderRadius: '50%', width: '40px', height: '40px'}}
+    src={`https://eth.vanity.show/${props.address}`}
+    alt={`Identicon of ether address ${props.address}`}
+  />
+)
 
-  render() {
-    return (<div>
-      <div className="col-xs-12 center">
-        <h1>
-          Logged as {this.props.accountAddress}
-        </h1>
-      </div>
-    </div>)
-  }
-}
+const LoggedInAccount = props => (
+  <div>
+  <div className="col-xs-12 center">
+    <h1>
+      Logged as <Identicon address={props.accountAddress} />
+    </h1>
+
+    <h3>My public address is</h3>
+    <p>
+      {props.accountAddress}
+    </p>
+  </div>
+</div>
+)
 
 const mapStateToProps = ({ login }) => ({
   accountAddress: login.accountAddress
