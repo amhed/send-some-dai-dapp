9c48f4b0d293d9dfd83844284004793547797aec
Amhed <amhed@harbor.com>
2018-09-08 23:41:39 +0200

    Add heroky post-build config

--- server.js
+++ server.js
@@ -106,4 +106,13 @@ app.get('/api/create-disposable-wallet', (req, res) => {
   }
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
