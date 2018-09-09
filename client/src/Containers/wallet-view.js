5f60eb76b5e519fc053ec6bde6eb25bddf83d603
Eduardo Sorribas <eduardo@sorribas.org>
2018-09-09 10:50:16 +0200

    Make wallet view a little prettier.

--- client/src/Containers/wallet-view.js
+++ client/src/Containers/wallet-view.js
@@ -23,17 +23,28 @@ class WalletView extends React.Component {
       <div>$<span className="balance-number">{this.props.walletBalance}</span></div>
       <input id="address-input" type="text" placeholder="Address to send" onChange={this.handleTransactionChange.bind(this)}/>
 
+
       <div>
 				<button id="bottom-container" onClick={() => this.props.transferWalletFunds(this.props.match.params.walletId, this.state.addressToSend, this.props.walletBalance)}>
 					Transfer
 				</button>
       </div>
 
+      {this.status()}
+
     </div>
+  }
+
+  status() {
+    if (this.props.transferStatus === 'done') return <div className="transfer-done">Transfer complete</div>
+    if (this.props.transferStatus === 'error')return  <div className="transfer-error">An error ocurred while transferring the funds</div>
+    if (!this.props.transferStatus) return null
+
   }
 }
 const mapStateToProps = ({ wallet }) => ({
-  walletBalance: wallet.walletBalance
+  walletBalance: wallet.walletBalance,
+  transferStatus: wallet.transferStatus
 })
 
 const mapDispatchToProps = dispatch =>
--- client/src/Reducers/wallet.js
+++ client/src/Reducers/wallet.js
@@ -1,7 +1,10 @@
 export const UPDATE_WALLET_BALANCE = 'wallet/REQUEST_BALANCE'
+export const WALLET_FUNDS_TRANSFERRED = 'wallet/WALLET_FUNDS_TRANSFERRED'
+export const WALLET_FUNDS_ERRORED = 'wallet/WALLET_FUNDS_ERRORED'
 
 const initialState = {
-  walletBalance: 0
+  walletBalance: 0,
+  transferStatus: null
 }
 
 export default (state = initialState, action) => {
@@ -10,6 +13,18 @@ export default (state = initialState, action) => {
       return {
         ...state,
         walletBalance: action.balance
+      }
+
+    case WALLET_FUNDS_TRANSFERRED:
+      return {
+        ...state,
+        transferStatus: 'done'
+      }
+
+    case WALLET_FUNDS_ERRORED:
+      return {
+        ...state,
+        transferStatus: 'error'
       }
 
     default:
@@ -29,7 +44,11 @@ export const updateWalletBalance = walletId => {
 
 export const transferWalletFunds = (walletId, dest, amount) => {
   return async dispatch => {
-    const transfer = await fetch(`/api/send-to/${walletId}/${dest}/${amount}`).then(x => x.json())
-    console.log(transfer)
+    try {
+      const transfer = await fetch(`/api/send-to/${walletId}/${dest}/${amount}`).then(x => x.json())
+      if (transfer.transactionHash) dispatch({ type: WALLET_FUNDS_TRANSFERRED })
+    } catch (e) {
+      dispatch({ type: WALLET_FUNDS_ERRORED })
+    }
   }
 }
