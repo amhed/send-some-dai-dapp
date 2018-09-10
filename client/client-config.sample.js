39e5dea876de549ad0c0d53fac58a41fcccb6673
Amhed <amhed@harbor.com>
2018-09-10 09:38:16 +0200

    Update client config files
    Formatting + ES6 syntax on oasis-direct.js

--- client/client-config.sample.js
+++ client/client-config.sample.js
@@ -1,2 +1,7 @@
 export default {
+  contractAddresses: {
+    oasis: '0x8cf1cab422a0b6b554077a361f8419cdf122a9f9',
+    weth: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
+    dai: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2'
+  }
 }
--- client/src/Modules/oasis-direct.js
+++ client/src/Modules/oasis-direct.js
@@ -1,100 +1,118 @@
-var web3 = window.web3 || {}
-var oasisDesc = require('./odcontract.json')
-var oasisAddress = '0x8cf1cab422a0b6b554077a361f8419cdf122a9f9'
-var oasis = web3.eth.contract(oasisDesc).at(oasisAddress);
+import { contractAddresses } from '../../client-config'
+const addresses = contractAddresses
 
-var wethAddress = '0xd0a1e359811322d97991e03f863a0c30c2cf029c'
-var erc20contract = require('./erc20contract.json')
-var weth = web3.eth.contract(erc20contract).at(wethAddress)
+const web3 = window.web3 || {}
+import oasisContractAbi from './odcontract.json'
+const oasis = web3.eth.contract(oasisContractAbi).at(addresses.oasis)
 
-var daiAddress = '0xc4375b7de8af5a38a93548eb8453a498222c4ff2'
-var dai = web3.eth.contract(erc20contract).at(daiAddress)
+import erc20contractAbi from './erc20contract.json'
+const weth = web3.eth.contract(erc20contractAbi).at(addresses.weth)
+const dai = web3.eth.contract(erc20contractAbi).at(addresses.weth)
 
-exports.buyDai = function (amount, cb) {
-  var transactionHash
-  oasis.buyAllAmount.sendTransaction(daiAddress, amount, wethAddress, 500000000000000000000, {
-    from: web3.eth.accounts[0],
-    gas:4000000
-  }, onbuy)
+export function buyDai(amount, cb) {
+  //TODO: use await instead? keeping track of variables is hard
+  let transactionHash
+  oasis.buyAllAmount.sendTransaction(
+    addresses.dai,
+    amount,
+    addresses.weth,
+    500000000000000000000,
+    {
+      from: web3.eth.accounts[0],
+      gas: 4000000
+    },
+    onbuy
+  )
 
-  function onbuy (err, _transactionHash) {
+  function onbuy(err, _transactionHash) {
     if (err) return cb(err)
     transactionHash = _transactionHash
     web3.eth.getTransaction(transactionHash, ontransaction)
   }
 
-  function ontransaction (err, transaction) {
+  function ontransaction(err, transaction) {
     if (err) return cb(err)
     if (transaction && transaction.blockNumber) return cb(null, transactionHash)
     setTimeout(() => web3.eth.getTransaction(transactionHash, ontransaction), 1000)
   }
 }
 
-exports.approveWeth = function (cb) {
-  var transactionHash
-  weth.approve.sendTransaction(oasisAddress, -1, {
-    from: web3.eth.accounts[0],
-    gas:4000000
-  }, onsend)
-  
-  function onsend (err, _transactionHash) {
+export function approveWeth(cb) {
+  let transactionHash
+  weth.approve.sendTransaction(
+    addresses.oasis,
+    -1,
+    {
+      from: web3.eth.accounts[0],
+      gas: 4000000
+    },
+    onsend
+  )
+
+  function onsend(err, _transactionHash) {
     if (err) return cb(err)
     transactionHash = _transactionHash
     web3.eth.getTransaction(transactionHash, ontransaction)
   }
 
-  function ontransaction (err, transaction) {
+  function ontransaction(err, transaction) {
     if (err) return cb(err)
     if (transaction && transaction.blockNumber) return cb(null, transactionHash)
     setTimeout(() => web3.eth.getTransaction(transactionHash, ontransaction), 1000)
   }
 }
 
-exports.buyWeth = function (val, cb) {
-  var transactionHash
-  weth.deposit.sendTransaction({
-    from: web3.eth.accounts[0],
-    gas:4000000,
-    value: val
-  }, ondeposit)
+export function buyWeth(val, cb) {
+  let transactionHash
+  weth.deposit.sendTransaction(
+    {
+      from: web3.eth.accounts[0],
+      gas: 4000000,
+      value: val
+    },
+    ondeposit
+  )
 
-  function ondeposit (err, _transactionHash) {
+  function ondeposit(err, _transactionHash) {
     if (err) return cb(err)
     transactionHash = _transactionHash
     web3.eth.getTransaction(transactionHash, ontransaction)
   }
 
-  function ontransaction (err, transaction) {
+  function ontransaction(err, transaction) {
     if (err) return cb(err)
     if (transaction && transaction.blockNumber) return cb(null, transactionHash)
     setTimeout(() => web3.eth.getTransaction(transactionHash, ontransaction), 1000)
   }
 }
 
-exports.transferDai = function (toAddr, amount, cb) {
-  var transactionHash
-  dai.transfer.sendTransaction(toAddr, amount, {
-    from: web3.eth.accounts[0],
-    gas:4000000
-  }, ontransfer)
+export function transferDai(toAddr, amount, cb) {
+  let transactionHash
+  dai.transfer.sendTransaction(
+    toAddr,
+    amount,
+    {
+      from: web3.eth.accounts[0],
+      gas: 4000000
+    },
+    ontransfer
+  )
 
-  function ontransfer (err, _transactionHash) {
+  function ontransfer(err, _transactionHash) {
     if (err) return cb(err)
     transactionHash = _transactionHash
     web3.eth.getTransaction(transactionHash, ontransaction)
   }
 
-  function ontransaction (err, transaction) {
+  function ontransaction(err, transaction) {
     if (err) return cb(err)
     if (transaction && transaction.blockNumber) return cb(null, transactionHash)
     setTimeout(() => web3.eth.getTransaction(transactionHash, ontransaction), 1000)
   }
 }
 
-exports.checkApproval = function (cb) {
-  var event = weth.Approval({src: web3.eth.accounts[0]}, {fromBlock: 0, toBlock: 'latest'})
-  event.get(function (err, evts) {
-    if (err) return cb(err)
-    cb(null, !!evts.length)
-  })
+export function checkApproval(cb) {
+  const event = weth.Approval({ src: web3.eth.accounts[0] }, { fromBlock: 0, toBlock: 'latest' })
+
+  event.get((err, evts) => (err ? cb(err) : cb(null, !!evts.length)))
 }
